f5ae0ba4f796bb35e62bc51f0e9230a4d6cf27ba
rodrimati1992 <rodrimatt1985@gmail.com>
2025-08-24 01:49:10 -0300

    Added test for comparison traits for TStr

--- tstr/tests/modules/tstr_methods_tests.rs
+++ tstr/tests/modules/tstr_methods_tests.rs
@@ -2,6 +2,8 @@ use crate::modules::utils::{assert_type, must_panic};
 
 use tstr::{IsTStr, TS, TStr};
 
+use std::cmp::{Ord, Ordering, PartialOrd};
+
 type AString = TS!(1234);
 type BString = TS!(12345);
 
@@ -29,4 +31,47 @@ fn conversion_from_generic_test() {
     }
 
     foo::<AString>();
+}
+
+#[test]
+fn cmp_test() {
+    #[track_caller]
+    fn case<L: IsTStr, R: IsTStr>(expected: Ordering) {
+        let lhs = TStr::<L::Arg>::new();
+        let rhs = TStr::<R::Arg>::new();
+
+        assert_eq!(
+            PartialOrd::partial_cmp(&lhs, &rhs),
+            Some(expected),
+            "partial_cmp"
+        );
+
+        assert_eq!(Ord::cmp(&lhs, &lhs), Ordering::Equal, "cmp");
+    }
+
+    case::<AString, AString>(Ordering::Equal);
+    case::<AString, BString>(Ordering::Less);
+    case::<BString, AString>(Ordering::Greater);
+    case::<BString, BString>(Ordering::Equal);
+}
+
+#[test]
+fn eq_ne_test() {
+    fn assert_is_eq<T: std::cmp::Eq>(_: T) {}
+
+    #[track_caller]
+    fn case<L: IsTStr, R: IsTStr>(expected: bool) {
+        let lhs = TStr::<L::Arg>::new();
+        let rhs = TStr::<R::Arg>::new();
+        assert_is_eq(lhs);
+        assert_is_eq(rhs);
+
+        assert_eq!(lhs == rhs, expected, "eq");
+        assert_eq!(lhs != rhs, !expected, "ne");
+    }
+
+    case::<AString, AString>(true);
+    case::<AString, BString>(false);
+    case::<BString, AString>(false);
+    case::<BString, BString>(true);
 }
